7d0775e66ac21d7fe787888457ca0570ea9afa50
K88 <kiddchantw@gmail.com>
2020-05-22 16:41:51 +0800

    feat readall where id =

--- controller/stores.php
+++ controller/stores.php
@@ -1,7 +1,10 @@
 <?php
 class Stores{   
 
+	public $id;
+
 	private $conn;
+
 
 	public function __construct($db){
 		$this->conn = $db;
@@ -17,10 +20,28 @@ class Stores{
 
 
 	function readAll(){	
-		$query_read_all = "SELECT st.s_id `stores_id`,so.name `store_name`,so.Address `store_address` , group_concat(t.name separator ',') `tag_name`,so.google_map_url `gps` FROM storesXtags st LEFT JOIN tags t ON st.t_id = t.id LEFT JOIN stores so on so.id = st.s_id GROUP BY st.s_id ORDER BY count(st.s_id) DESC" ;
+		//$query_read_all = "SELECT st.s_id `stores_id`,so.name `store_name`,so.Address `store_address` , group_concat(t.name separator ',') `tag_name`,so.google_map_url `gps` FROM storesXtags st LEFT JOIN tags t ON st.t_id = t.id LEFT JOIN stores so on so.id = st.s_id GROUP BY st.s_id ORDER BY count(st.s_id) DESC" ;
+
+		$query_read_all = "" ;
+		$query_read_part1 = "SELECT st.s_id `stores_id`,so.name `store_name`,so.Address `store_address` , group_concat(t.name separator ',') `tag_name`,so.google_map_url `gps` FROM storesXtags st LEFT JOIN tags t ON st.t_id = t.id LEFT JOIN stores so on so.id = st.s_id ";
+		$query_read_part2 = "GROUP BY st.s_id ORDER BY count(st.s_id) DESC ";
+		$query_id = $this->id;
+
+		if ($query_id == 0){
+			$query_where = "";
+			$query_read_all = $query_read_part1.$query_read_part2 ;
+
+		}else{
+			$query_where = " where st.s_id = $query_id ";
+			$query_read_all =  $query_read_part1.$query_where.$query_read_part2 ; 
+		}
+
+
+
 		$stmt = $this->conn->prepare($query_read_all);	
 		$stmt->execute();
 		return $stmt;
+		
 	}
 
 
